31dfd2a2b2f53c8234dbfbdb3b0fea8a1a40eb6c
sartfromua <82293950+sartfromua@users.noreply.github.com>
2021-10-28 19:49:32 +0300

    Stuctures for project
    Как происходит наследование структур на чистом C? Как должно правильно выглядеть обращение к параметру класса Person, например через класс Employee?

--- main.c
+++ main.c
@@ -1,8 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+
+typedef struct Date {
+    int day, month, year;
+} Date;
+
+
+typedef struct Person {
+    char name[], surname[];
+    char sex[];
+    Date birthday;
+} Person;
+
+
+typedef struct Product {
+    char product_name[];
+    double price;
+} Product;
+
+
+typedef struct Equipment {
+    char equipment_name[];
+    Product product;
+} Equipment;
+
+
+typedef struct Profession {
+    char profession_name[];
+    double wage_rate;
+    Equipment equipment;
+} Profession;
+
+
+typedef struct Employee {
+    struct Person;
+    struct Profession;
+    double salary;
+    Date employed;
+    Profession profession;
+} Employee;
+
+
+typedef struct Deputy {
+
+} Deputy;
+
+
+typedef struct Head {
+
+} Head;
+
+
+int main() {
+    Employee *emp;
+    emp.Person.sex = "male";
+    printf("%s", emp.Person.sex);
+//    emp.sex = "male";       ??? What example is right? How is it supposed to be?
+//    printf("%s", empsex);   ???
 }
