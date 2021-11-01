315904eb8e407363ccf892fa19c135de1d377539
sartfromua <82293950+sartfromua@users.noreply.github.com>
2021-11-01 19:12:24 +0200

    Поправил структуры, работают
    Какие параметры классов нужно изменить/добавить?

---
+++ structures.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct {
+    int day, month, year;
+} date;
+
+
+typedef struct {
+    char name[255], surname[255];
+    char sex[255];
+    date birthday;
+} Person;
+
+
+typedef struct {
+    char name[255];
+    double price;
+} Product;
+
+
+typedef struct {
+    char name[255];
+    Product product;
+} Equipment;
+
+
+typedef struct {
+    char name[255];
+    double wage_rate;
+    Equipment equipment;
+} Profession;
+
+
+typedef struct {
+    Person person;
+    Profession proffesion;
+    double salary;
+    date employed;
+} Employee;
+
+
+typedef struct {
+    Person person;
+    double salary;
+    date employed;
+} Deputy;
+
+
+typedef struct {
+    Person person;
+    double salary;
+    date employed;
+} Head;
